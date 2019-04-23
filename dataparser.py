1d328f06f544f56e49349c1d9234f70872780e1d
AnnaZazulya <zinoborsh2@mail.ru>
2019-04-23 19:59:03 +0300

    + функция 'что сегодня'

--- dataparser.py
+++ dataparser.py
@@ -33,7 +33,7 @@ name_month = {'янв': '%D1%8F%D0%BD%D0%B2%D0%B0%D1%80%D1%8F', 'фев': '%D1%8
               'сентября': '%D1%81%D0%B5%D0%BD%D1%82%D1%8F%D0%B1%D1%80%D1%8F',
               'октяьря': '%D0%BE%D0%BA%D1%82%D1%8F%D0%B1%D1%80%D1%8F',
               'ноября': '%D0%BD%D0%BE%D1%8F%D0%B1%D1%80%D1%8F',
-              'декабря': '%D0%B4%D0%B5%D0%BA%D0%B0%D0%B1%D1%80%D1%8F',}
+              'декабря': '%D0%B4%D0%B5%D0%BA%D0%B0%D0%B1%D1%80%D1%8F'}
 
 count_months = {'янв': 1, 'фев': 2, 'мар': 3, 'апр': 4, 'май': 5, 'июн': 6,
                 'июл': 7, 'авг': 8, 'сен': 9, 'окт': 10, 'ноя': 11, 'дек': 12,
@@ -41,6 +41,9 @@ count_months = {'янв': 1, 'фев': 2, 'мар': 3, 'апр': 4, 'май': 5,
                 'июля': 7, 'августа': 8, 'сентября': 9, 'октября': 10, 'ноября': 11, 'декабря': 12,
                 'январь': 1, 'февраль': 2, 'март': 3, 'апрель': 4, 'июнь': 6,
                 'июль': 7, 'август': 8, 'сентябрь': 9, 'октябрь': 10, 'ноябрь': 11, 'декабрь': 12}
+
+reverse_count_months = {1: 'января', 2: 'февраля', 3: 'марта', 4: 'апреля', 5: 'мая', 6: 'июня',
+                        7: 'июля', 8: 'августа', 9: 'сентября', 10: 'октября', 11: 'ноября', 12: 'декабря'}
 
 that_year = datetime.datetime.today().year
 
@@ -74,7 +77,11 @@ else:
     print('невисокос')
 
 
-if len(data) == 3:
+if 'сегодня' in data:
+    print('https://ru.wikipedia.org/wiki/', datetime.datetime.today().day, '_',
+          name_month[reverse_count_months[datetime.datetime.today().month]], sep='')
+
+elif len(data) == 3:
     if 'до' in data:
         data.remove('до')
     else:
@@ -85,6 +92,7 @@ if len(data) == 3:
         my_event = event.replace(year=today.year + 1)
     time_to_event = abs(my_event - today)
     print('До события:', time_to_event.days, 'дней')
+
 else:
     if int(data[-0]) > 0 and data[-1] in months:
         if isint(data[-0]) == True and isstr(data[-1]) == True:
@@ -98,6 +106,7 @@ else:
         print('data < 0 или нет значения в словаре')
 
 
+#print('https://ru.wikipedia.org/wiki/', datetime.datetime.today().day, '_', name_month[datetime.datetime.today().month])
 #print(time.ctime())
 #print(months[data[1]])
 
